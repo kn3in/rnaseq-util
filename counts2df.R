0ee6071764e0fc90499f56e57535683e04b4bce7
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2012-10-15 09:35:22 +1000

    counts2df: collect htseq-counts output into a file, DESeq/edgeR ready

---
+++ counts2df.R
@@ -0,0 +1,24 @@
+#!/hox/g/uq/sw/x86_64/R/2.15.1/bin/Rscript
+
+# aggregate htseq-counts output into a data frame
+# discard genes with 0 counts across all samples
+# save resulting dataframe as 'my_counts.csv' file
+
+files <- dir(pattern = "_count.txt")
+names <- as.vector(sapply(files, function(x) strsplit(x, "_count.txt")[[1]]))
+data <- lapply(files, read.table, sep = "\t", header = FALSE)
+# drop last 5 summary rows
+data <- lapply(data, function(x) head(x, -5))
+names(data) <- names
+
+data <- lapply(names, function(x) {
+  colnames(data[[x]])[2] <- x
+  data[[x]] 
+})
+
+data <- Reduce(function(x, y) merge(x, y, by.x = "V1", by.y = "V1"), data, accumulate = FALSE)
+names(data)[1] <- "gene_id"
+data$filter <- rowSums(data[ ,-1])
+data <- subset(data, filter != 0)
+data$filter <- NULL
+write.csv(data, file = "my_counts.csv", quote = FALSE, row.names = FALSE)
